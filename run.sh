f837304ba8c967782898f801c9ce6a887a25871e
Dan Piet <daniel.piet@rokt.com>
2023-09-08 11:47:30 -0400

    add macbook securty get-generic-password for become pass

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-ansible-playbook -i hosts site.yml --ask-become-pass
+
+BECOME_PASS=$(security find-generic-password -w -s 'civil-digital-ocean' -a piet)
+
+ansible-playbook -i hosts site.yml --extra-vars "ansible_become_pass=$BECOME_PASS"
